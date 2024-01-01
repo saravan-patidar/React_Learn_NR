94ee98d20b974058e1b857942934ad6ff831dfb4
saravan-patidar <shravanpatidar.48@gmail.com>
2024-01-01 18:36:47 +0530

    modified:   package.json 	modified:   src1/app.js

--- src1/app.js
+++ src1/app.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// const heading = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+// ]);
+
+const jsxElem = <h1>Hello ,this is jsx.. </h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxElem);
